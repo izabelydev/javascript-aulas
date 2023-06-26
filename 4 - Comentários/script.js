76245a193c02bd5c49727209c27eb9dd9baa8e85
izalrnc <izabelylrnc@gmail.com>
2023-06-26 20:52:15 -0300

    Maneiras de comentar | linha e bloco de comentário

---
+++ 4 - Comentários/script.js
@@ -0,0 +1,16 @@
+// comentario com barras é para linha
+console.log('olá mundo'); //outro comentario
+
+/*
+barra e asterisco para comentario em bloco 
+
+console.log('linha 1');
+console.log('linha 2');
+console.log('linha 3');
+*/
+
+/** comentario com varios asteriscos
+ * 
+ * 
+ * exemplo
+ */
