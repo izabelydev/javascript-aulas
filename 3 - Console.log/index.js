d101e1b1e076855e7320a25afa76afe9787f4b59
izalrnc <izabelylrnc@gmail.com>
2023-06-26 20:29:02 -0300

    console.log | string e number

---
+++ 3 - Console.log/index.js
@@ -0,0 +1,9 @@
+console.log('Izabely "V.B." Lourenço'); // pode usar aspas simples quando o seu texto tiver aspas duplas
+console.log("Izabely 'V.B.' Lourenço"); //pode usar aspas duplas quando texto tiver aspas simples
+console.log(`Izabely "V." 'B.' Lourenço`); //usado para fazer template, pode usar aspas simples ou duplas
+
+// o valor dentro das aspas é considerado string, fora das aspas é number
+// essa função faz a quebra de linha na saída
+
+console.log(123456, 14.6734456); // números não usam aspas
+console.log(12, 67.54, 'IzaLouCraft');
