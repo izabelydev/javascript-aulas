c76b8241ef7c2067c7b7b637d50555c552f18967
izalrnc <izabelylrnc@gmail.com>
2023-07-11 20:21:30 -0300

    Aula sobre declarar variaveis usando o comando let

---
+++ 6-variaveis-let/script.js
@@ -0,0 +1,23 @@
+let nome = 'Henrique'; //string
+// o comando 'var' é mais antigo, por isso usamos 'let'
+
+console.log(nome, ' nasceu em 1984');
+console.log('Em 2000 ', nome, ' conheceu Maria');
+console.log(nome, ' casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com ', nome, ' em 2015');
+console.log('O filho de ', nome, ' se chama Eduardo');
+
+let nome2; //Declarou a variável
+nome2 = 'Iza'; //Inicializou a variável
+console.log(nome2);
+nome2 = 'Marcos';
+console.log(nome2);
+
+/*
+Regras para criar variáveis:
+-> Não podemos criar variáveis com palavras reservadas (palavras usadas nos comandos da linguagem. Ex: let, console, etc)
+-> Variaveis precisam ter nomes significativos.Ex: let nomeCliente;
+-> Não pode começar o nome da variavel com número
+-> Nome da variavel nao pode conter traços ou espaços, mas pode usar cameCase (primeira letra minuscula, as demais palavras com letra maiuscula)
+-> Em JavaScript as variaveis são case sensitive
+*/
