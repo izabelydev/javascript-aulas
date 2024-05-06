2bbbd4d106061a6c004a0e9d2a67b5cf5474e155
izabelydev <izabelylrnc@gmail.com>
2024-05-06 07:17:11 -0300

    Exercício: Somar os valores de um prompt (converta para number) e no final coloque um alert com o resultado da adição

--- 10 - alert_confirm_prompt/exercício/js/script.js
+++ 10 - alert_confirm_prompt/exercício/js/script.js
@@ -1,1 +1,7 @@
 //Somar os valores de um prompt (converta para number) e no final coloque um alert com o resultado da adição
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite outro número:');
+
+soma = parseFloat(num1) + parseFloat(num2);
+
+alert('A soma dos números é: ' + soma);
