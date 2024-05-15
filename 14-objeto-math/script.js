3f3e3d5241de7749d6b3cd9fb08646a6acdc033a
izabelydev <izabelylrnc@gmail.com>
2024-05-15 20:12:24 -0300

    Aula sobre objeto Math: arredondamento, raiz quadrada, potencia

---
+++ 14-objeto-math/script.js
@@ -0,0 +1,30 @@
+// Arredondar para baixo
+let num1 = Math.floor(9.54578);
+console.log(num1);
+
+// Arredondar para cima
+let num2 = Math.ceil(9.54578);
+console.log(num2);
+
+// Arrendondar para cima ou para baixo
+let num3 = Math.round(9.501);
+console.log(num3);
+
+// Mostrar o maior número da lista
+console.log(Math.max(1,2,3,4,-1,100,2,6,7));
+
+// Mostrar o menor número da lista
+console.log(Math.min(1,2,3,4,-1,100,2,6,7));
+
+// Gerar números aleatórios entre 0 e 1
+console.log(Math.random());
+
+// Exemplo entre 5 e 10 com números inteiros
+const aleatorio = Math.round(Math.random() * (10 -5) + 5);
+console.log(aleatorio);
+
+// Elevar ao quadrado
+console.log(2 ** 10);
+
+// Raiz quadrada
+console.log(9 ** 0.5);
