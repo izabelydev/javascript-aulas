af44d3c36ab054cd5b359cd799371070ef993c2d
izabelydev <izabelylrnc@gmail.com>
2024-06-19 21:57:25 -0300

    Botões com interação e conteúdo dinâmico

---
+++ 16-funcoes-basicas/Desafio HTML dinâmico/script.js
@@ -0,0 +1,20 @@
+const criarCardInput = document.getElementById('criarCardInput');
+const mostrarCardInput = document.getElementById('mostrarCardInput');
+
+function criarCard () {
+    return criarCardInput.innerHTML = `
+        <input id="titulo" type="text" placeholder="Título">
+        <input id="descricao" type="text" placeholder="Descrição">
+        <button onclick="mostrarCard()">Salvar</button>`;
+}
+
+function mostrarCard() {
+    const titulo = document.getElementById('titulo').value;
+    const descricao = document.getElementById('descricao').value;
+
+    return mostrarCardInput.innerHTML = `
+            <div style="width: 500px; height: 500px; background-color: blue; color: #ffffff;">
+                <h1 class="titulo">${titulo}</h1>
+                <p class="descricao">${descricao}</p>
+            </div>`;
+}
