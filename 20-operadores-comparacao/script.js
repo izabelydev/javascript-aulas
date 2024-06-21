c93259443c867dd8b3e58ea4db9369fc3b7979df
izabelydev <izabelylrnc@gmail.com>
2024-06-20 23:39:00 -0300

    Operadores de comparação: > | >= | < | <= | == | === | != | !==

---
+++ 20-operadores-comparacao/script.js
@@ -0,0 +1,27 @@
+/*
+Operadores comparação
+> maior que
+>= maior ou igual que
+< menor que
+<= menor ou igual que
+= atribuição
+== igualdade (checar valor) ** não recomendado
+=== igualdade estrita (valor e tipo)
+!= diferente (checar valor) ** não recomendado
+!== diferente estrito (valor e tipo)
+*/ 
+const num1 = 10;
+const num2 = 11;
+let comp = num1 < num2;
+comp = num1 === num2;
+comp = num1 !== num2;
+console.log(comp);
+
+
+
+
+
+
+
+
+
