7184377fceed4f1fe4d8bbfdda9644c9386351e7
izabelydev <izabelylrnc@gmail.com>
2024-06-26 17:42:41 -0300

    Exercício: data e hora

---
+++ 27-objeto-date/exercicio/assets/js/script.js
@@ -0,0 +1,28 @@
+const dateDocumentH1 = document.querySelector('.date');
+const dateDocumentH3 = document.querySelector('.hour');
+const date = new Date();
+
+// Declaração de variáveis
+const week = date.getDay();
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+const hour = date.getHours();
+const min = date.getMinutes();
+
+// Nomeação de variáveis
+const weekName = [
+    'Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'
+];
+const monthName = [
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+
+function leftZero (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+dateDocumentH1.innerHTML = `${weekName[week]}, ${day} de ${monthName[month]} de ${year}`;
+dateDocumentH3.innerHTML = `${hour}:${min}`
+
+
