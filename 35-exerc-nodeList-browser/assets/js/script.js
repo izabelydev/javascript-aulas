7fee81a43ca471c15c51854f989d0569684dc356
izabelydev <132600516+izabelydev@users.noreply.github.com>
2024-07-01 15:01:28 -0300

    Update script.js

--- 35-exerc-nodeList-browser/assets/js/script.js
+++ 35-exerc-nodeList-browser/assets/js/script.js
@@ -1,5 +1,5 @@
 const paragrafos = document.querySelector('.paragrafos');
-const todosParagrafos = paragrafos.querySelectorAll('p');
+const todosParagrafos = paragrafos.querySelectorAll('p'); // nodeList - semelhante a um array, mas não é necessariamente um array
 
 const estilosBody = getComputedStyle(document.body);
 const backgroundColorBody = estilosBody.backgroundColor;
