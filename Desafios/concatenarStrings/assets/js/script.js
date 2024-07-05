3de6d6d9b8fd0788f73365ed9e04b625338d617c
izabelydev <izabelylrnc@gmail.com>
2024-07-05 09:38:09 -0300

    Finalização de animações e concatenação de strings.

--- Desafios/concatenarStrings/assets/js/script.js
+++ Desafios/concatenarStrings/assets/js/script.js
@@ -1,8 +1,27 @@
 const form = document.querySelector('.form');
 const response = document.querySelector('.response');
+const h1 = document.querySelector('.dinamicName');
+const p = document.querySelector('.dinamicAgeAndCity');
 const button = document.querySelector('.concludeBt');
+const arrowBack = document.querySelector('.material-symbols-outlined');
 
 button.addEventListener('click', () => {
-    form.classList.remove('active');
-    response.classList.add('active');
+    const name = document.querySelector('#name').value;
+    const age = document.querySelector('#age').value;
+    const city = document.querySelector('#city').value;
+
+    if (name === '' || age === '' || city === '') {
+        window.alert('Preencha todos os campos!');
+    } else {
+        h1.innerHTML = `Olá ${name}`;
+        p.innerHTML = `Você tem ${age} anos, e mora em ${city}.<br>
+        Obrigado, até logo!`;
+        form.classList.remove('active');
+        response.classList.add('active');
+    }
+});
+
+arrowBack.addEventListener('click', () => {
+    form.classList.add('active');
+    response.classList.remove('active');
 });
