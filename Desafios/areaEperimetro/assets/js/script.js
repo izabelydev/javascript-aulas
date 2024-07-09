798b5a32cd0ea89b25c1dd78f838854d3fbd4976
izabelydev <izabelylrnc@gmail.com>
2024-07-09 11:01:46 -0300

    Organização do arquivo style.css e correções finais no arquivo script.js

--- Desafios/areaEperimetro/assets/js/script.js
+++ Desafios/areaEperimetro/assets/js/script.js
@@ -6,28 +6,28 @@ const btArea = document.querySelector('.area');
 const btPerimetro = document.querySelector('.perimetro');
 const btCalcular = document.querySelector('.btCalcular');
 const p = document.querySelector('.titulo');
+let resultado;
 
-// Capturando o primeiro evento dos botões A ou P para abrir o form
+// Captur o primeiro evento dos botões Area ou Perimetro para abrir o form
 document.addEventListener('click', (elemento) => {
     const el = elemento.target;
 
-    if(el.classList.contains('area')) {
+    if (el.classList.contains('area')) {
+        form.classList.add('active');
         container.classList.remove('plus');
         form.classList.remove('perimetro');
         form.classList.add('area');
         criarForm('area');
     } else if (el.classList.contains('perimetro')) {
+        form.classList.add('active');
         container.classList.remove('plus');
         form.classList.remove('area');
         form.classList.add('perimetro');
         criarForm('perimetro');
     }
 
-    try {
-        container.removeChild(divSelecionar);   
-    } catch (error) {
-        console.log('Já removido');
-    }
+    // remover div inicial
+    container.removeChild(divSelecionar);
 
 });
 
@@ -48,35 +48,42 @@ function criarForm(classe) {
     // Botão calcular
     const btCalcular = document.querySelector('.btCalcular');
     btCalcular.addEventListener('click', calcular);
-} 
+}
 
 // Função calcular a área ou o perímetro
 function calcular() {
+    // Declaração de variáveis dos inputs
     const inputComprimento = document.querySelector('.comprimento').value;
     const inputLargura = document.querySelector('.largura').value;
-    let resultado;
 
     // identificar tipo de calculo à ser executado
-    if(form.classList.contains('area')) {
+    if (inputComprimento === '' || inputLargura === '') {
+        alert('Preencha todos os campos');
+    } else if (form.classList.contains('area')) {
         const area = parseFloat(inputComprimento) * parseFloat(inputLargura);
         resultado = area;
+        mostrarResultado();
     } else if (form.classList.contains('perimetro')) {
         const perimetro = (parseFloat(inputComprimento) + parseFloat(inputLargura)) * 2;
         resultado = perimetro;
+        mostrarResultado();
     }
 
+}
+
+function mostrarResultado() {
     // Criar elemento para mostrar o resultado
     const h1Resultado = document.createElement('h1');
     h1Resultado.classList.add('resultado', 'show');
     h1Resultado.innerText = `${resultado}m²`;
 
-    // apagar resultado anterior (se tiver)
+    // Apagar resultado anterior (se tiver)
     const resultadoAnterior = document.querySelector('.resultado');
     if (resultadoAnterior) {
         container.removeChild(resultadoAnterior);
     }
 
-    // adicionar elemento com o resultado no container
+    // Adicionar elemento com o resultado no container
     container.appendChild(h1Resultado);
     container.classList.add('plus');
 }
