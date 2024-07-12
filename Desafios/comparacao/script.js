64d057034a63f9dec38ae30f7d79a0c749a9f6ab
izabelydev <izabelylrnc@gmail.com>
2024-07-11 21:17:43 -0300

    Comparar se o número X é menor, maior ou igual ao número Y

---
+++ Desafios/comparacao/script.js
@@ -0,0 +1,8 @@
+// Comparar se um número é maior, menor ou igual ao segundo número
+
+const x = 3;
+const y = 5;
+
+const resultado = x === y ? `${x} igual a ${y}` : x > y ? `${x} maior que ${y}` : `${x} menor que ${y}`;
+
+console.log(resultado);
