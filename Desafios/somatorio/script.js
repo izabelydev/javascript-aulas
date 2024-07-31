85da83dd5dc284bb7e11fb5d213c18d5f189f599
izabelydev <izabelylrnc@gmail.com>
2024-07-31 17:16:45 -0300

    Somatório de dois números

---
+++ Desafios/somatorio/script.js
@@ -0,0 +1,13 @@
+// Criar um programa que calcula a soma de números de 1 a N
+
+const n = 2;
+let i = 0;
+let soma = 0;
+
+while (i <= n) {
+    soma += i;
+    i++;
+}
+
+console.log(`O somátorio é ${soma}`);
+
